d96ee3f6d010837c582aed52436990b8edb222a8
David <david@ortner.se>
2024-01-18 20:45:08 +0100

    Initial commit.

---
+++ polyfills/url.ts
@@ -0,0 +1,8 @@
+import { URL, URLSearchParams } from 'whatwg-url';
+/**
+ *
+ */
+class UrlObject {}
+
+export { URL, URLSearchParams, UrlObject };
+export default { URL, URLSearchParams, UrlObject };
---
+++ polyfills/vm.ts
@@ -0,0 +1,36 @@
+type Context = object;
+
+/**
+ *
+ */
+class Script {
+	private code: string;
+
+	/**
+	 *
+	 * @param code
+	 */
+	constructor(code: string) {
+		this.code = code;
+	}
+	/**
+	 *
+	 * @param context
+	 */
+	public runInContext(context: Context): void {
+		const evaluate = (code: string): void => {
+			globalThis.eval(code);
+		};
+		evaluate.call(context, this.code);
+	}
+}
+const contextSymbol = Symbol('context');
+const isContext = (context: Context): boolean => {
+	return context[contextSymbol] === true;
+};
+const createContext = (context: Context): Context => {
+	context[contextSymbol] = true;
+	return context;
+};
+export { Script, isContext, createContext };
+export default { Script, isContext, createContext };
